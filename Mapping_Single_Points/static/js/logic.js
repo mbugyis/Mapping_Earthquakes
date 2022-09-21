25c7efd5ae14f6d28d555b3127a55b8b5004799d
Michael <mbugyis@butler.edu>
2022-09-21 16:34:02 -0400

    Idk why this is making me do this

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,12 +5,13 @@ console.log("working");
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
+let marker = L.circle([34.0522, -118.2437], {
     radius: 300,
     color: "black",
     fillColor: "yellow"
 }).addTo(map);
 
+L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
